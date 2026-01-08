d4b9b227fa5c8d800f2b86c2423df8714777da7f
RoastedChestnut <chestnut.roasted@outlook.com>
2026-01-08 16:54:22 +0800

    fix: use correct path

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule MortalDrinksElixir.MixProject do
       app: :mord_ex,
       version: "0.1.0",
       build_path: "../_build",
-      config_path: "../config/config.exs",
+      config_path: "../config.exs",
       deps_path: "../deps",
       lockfile: "../mix.lock",
       elixir: "~> 1.19",
